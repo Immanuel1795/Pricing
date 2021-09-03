1b1b453475bef4d7ef46c1ddfdb9c97c50a97f17
immanuel antony <immanuelantoni177@gmail.com>
2021-09-03 20:24:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import priceArray from './price';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <section className="pricing py-5" >
+  <div className="container">
+    <div className="row">
+     {priceArray.map((details, index)=>{
+     return <div className="col-lg-4">
+        <div className="card mb-5 mb-lg-0">
+          <div className="card-body">
+            <h5 className="card-title text-muted text-uppercase text-center">{details.plan}</h5>
+            <h6 className="card-price text-center">{details.rate}<span className="period">/month</span></h6>
+            <hr />
+            <ul className="fa-ul">
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.user !=="Single User" ? <b>{details.user}</b> : details.user}</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.storage}</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.publicProjects}</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.access}</li>
+              {details.restricted === true ?           
+                 <><li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{details.privateProjects}</li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{details.phoneSupport}</li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{details.subDomain}
+              </li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{details.statusReports}</li> </> :  
+              
+              details.partiallyRestricted === true ? <><li><span className="fa-li"><i className="fas fa-check"></i></span>{details.privateProjects}</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.phoneSupport}</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.subDomain}</li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{details.statusReports}</li> </> : 
+
+              details.restricted === false ? <><li><span className="fa-li"><i className="fas fa-check"></i></span>{details.privateProjects}</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.phoneSupport}</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><b>Unlimited </b>{details.subDomain}</li>
+
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.statusReports}</li>
+               </> : null
+              
+              }
+              
+            </ul>
+            <div className="d-grid">
+              <a href="#" className="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
+     })}
+      
     </div>
+  </div>
+</section>
+
+    </div>
+
   );
 }
 
---
+++ src/price.js
@@ -0,0 +1,44 @@
+const priceArray = [{
+    plan: "FREE",
+    rate: "$0",
+    user: "Single User",
+    storage: "5GB Storage",
+    publicProjects: "Unlimited Public Projects",
+    access: "Community Access",
+    privateProjects: "Unlimited Private Projects",
+    phoneSupport: "Dedicated Phone Support",
+    subDomain: "Free Subdomain",
+    statusReports: "Monthly Status Reports",
+    restricted: true
+},
+{
+    plan: "PLUS",
+    rate: "$9",
+    user: "5 Users",
+    storage: "50GB Storage",
+    publicProjects: "Unlimited Public Projects",
+    access: "Community Access",
+    privateProjects: "Unlimited Private Projects",
+    phoneSupport: "Dedicated Phone Support",
+    subDomain: "Free Subdomain",
+    statusReports: "Monthly Status Reports",
+    partiallyRestricted: true
+},
+{
+    plan: "PRO",
+    rate: "$49",
+    user: "Unlimited Users",
+    storage: "150GB Storage",
+    publicProjects: "Unlimited Public Projects",
+    access: "Community Access",
+    privateProjects: "Unlimited Private Projects",
+    phoneSupport: "Dedicated Phone Support",
+    subDomain: "Free Subdomain",
+    statusReports: "Monthly Status Reports",
+    restricted: false
+}
+
+
+]
+
+export default priceArray;
